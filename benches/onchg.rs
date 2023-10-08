f24add8871917ed2e9ae46c88584b3d8329bb335
Assil Ksiksi <cyph0nik@gmail.com>
2023-10-08 07:26:53 -0400

    chore: Add basic grep bench to compare with

--- benches/onchg.rs
+++ benches/onchg.rs
@@ -1,4 +1,4 @@
-use criterion::{BenchmarkId, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use onchg::test_helpers::*;
 use onchg::Parser;
@@ -16,6 +16,17 @@ pub fn directory(c: &mut Criterion) {
             Parser::from_directory(d.path(), true).unwrap();
         });
     });
+    c.bench_with_input(BenchmarkId::new("grep", num_files), &d, |b, d| {
+        b.iter(|| {
+            let mut cmd = std::process::Command::new("grep");
+            cmd.current_dir(d.path())
+                .args(&["-rE", "LINT.OnChange|LINT.ThenChange", "."])
+                .stdout(std::process::Stdio::null());
+            cmd.spawn().unwrap().wait().unwrap();
+        });
+    });
+
+    drop(d);
 
     let d = TestDir::new();
     let mut f = RandomOnChangeTree::new(d.path().to_owned(), SEED, 5, 100, 100);
@@ -25,6 +36,15 @@ pub fn directory(c: &mut Criterion) {
     c.bench_with_input(BenchmarkId::new("directory", num_files), &d, |b, d| {
         b.iter(|| {
             Parser::from_directory(d.path(), true).unwrap();
+        });
+    });
+    c.bench_with_input(BenchmarkId::new("grep", num_files), &d, |b, d| {
+        b.iter(|| {
+            let mut cmd = std::process::Command::new("grep");
+            cmd.current_dir(d.path())
+                .args(&["-rE", "LINT.OnChange|LINT.ThenChange", "."])
+                .stdout(std::process::Stdio::null());
+            cmd.spawn().unwrap().wait().unwrap();
         });
     });
 }
